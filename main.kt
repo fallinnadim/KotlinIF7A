7e829914fead360527ad64f51625e204238427e7
fallinnadim <102505195+fallinnadim@users.noreply.github.com>
2023-10-03 08:59:07 +0700

    Create main.kt untuk pertemuan 7

---
+++ main.kt
@@ -0,0 +1,58 @@
+// null exception
+// variabel boleh bernilai null
+// semua tipe data boleh null
+
+fun main () {
+    var name = "Jafar Doe"
+    var age = 24
+    var occupation = ""
+    // Sentinel Value
+    // var errorCode = 0
+
+
+    var errorCode: Int? // nullable Int
+    errorCode = null  // set nilai menjadi null
+
+    // Checking for null
+    var result: Int? = 30
+    println(result!! + 1) // operator not-null assertion !!, untuk melakukan operasi aritmatika pada nullable Int
+
+    var authorName: String? = "Kotaro Minami"
+    var authorAge: Int? = 24
+    val ageAfterBirthday = authorAge!! + 1
+    println("After next birthday, author will be ${ageAfterBirthday}")
+
+    // not null assertion hanya untuk variable dengan nilai selain null
+
+    // Null checking dengan Smart Casts, safe call, let function, elvis operator
+
+    // Smart Casts
+    var nonNullableAuthor: String
+    var nullableAuthor: String?
+
+    if (authorName != null) {
+        nonNullableAuthor = authorName
+    }
+    else {
+        nullableAuthor = authorName
+    }
+
+    // Safe Call
+    // Operator ?. digunakan untuk mengecek apakah nilainya null atau tidak
+    // bisa dilakukan chain function selama nilainya tidak null
+    authorName = "Takeo Ischi"
+    var nameLength = authorName?.length
+    println("Author`s name has length ${nameLength} .")
+
+    var nameLengthPlus5 = authorName?.length?.plus(5)
+
+    // let() function
+    // let() function lebih fleksibel
+    authorName?.let {
+        nonNullableAuthor = authorName
+    }
+
+    // Elvis Operator (?:)
+    var nullableInt : Int? = 10
+    var mustHaveResult = nullableInt ?: 13 // assign value dengan nilai 13, jika nilai variable nullableInt = null
+}
