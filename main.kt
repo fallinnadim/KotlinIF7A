9b9edda98f4d5e96cf39320801069e86ce33b116
fallinnadim <102505195+fallinnadim@users.noreply.github.com>
2023-10-21 11:17:30 +0700

    Pertemuan 12

---
+++ main.kt
@@ -0,0 +1,79 @@
+// prinsip design pattern singleton
+// memastikan hanya ada satu instansi pada suatu program
+
+data class Student(val id: Int, val firstName: String, val lastName: String) {
+    var fullName = "${lastName}, ${firstName}"
+}
+
+object StudentRegistry {
+    val allStudents = mutableListOf<Student>()
+
+    fun addStudent(student: Student) {
+        if (allStudents.contains(student))
+            return
+        allStudents.add(student)
+    }
+    fun removeStudent(student: Student) {
+        allStudents.remove(student)
+    }
+    fun listAllStudents(){
+        allStudents.forEach{
+            println(it.fullName)
+        }
+    }
+}
+
+// Private constructor tidak bisa diakses diluar class
+class Scientist private constructor(val id: Int, val firstName: String, val lastName: String) {
+    // Companion object serupa dengan static pada Java
+    companion object {
+        var currentId = 0
+        fun newScientist(firstName: String, lastName: String): Scientist {
+            currentId += 1
+            return Scientist(currentId, firstName, lastName)
+        }
+    }
+    var fullName = "${firstName} ${lastName}"
+}
+
+object ScientistRepository {
+    val allScientist = mutableListOf<Scientist>()
+    fun addScientist(scientist: Scientist) {
+        if (allScientist.contains(scientist))
+            return
+        allScientist.add(scientist)
+    }
+    fun removeStudent(scientist: Scientist) {
+        allScientist.remove(scientist)
+    }
+    fun listAllScientist() {
+        allScientist.forEach{
+            println("${it.id} : ${it.fullName}")
+        }
+    }
+
+
+}
+
+fun main() {
+    // Singletons
+    // object
+    val marie = Student(1, "Marie", "Curie")
+    val albert = Student(2, "Albert", "Einstein")
+    val richard = Student(3, "Richard", "Feynman")
+
+    StudentRegistry.addStudent(marie)
+    StudentRegistry.addStudent(albert)
+    StudentRegistry.addStudent(marie)
+    StudentRegistry.listAllStudents()
+
+    val emmy = Scientist.newScientist("Emmy", "Noether")
+    val isaac = Scientist.newScientist("Isaac", "Newton")
+    val nick = Scientist.newScientist("Nikola", "Tesla")
+
+    ScientistRepository.addScientist(emmy)
+    ScientistRepository.addScientist(isaac)
+    ScientistRepository.addScientist(nick)
+
+    ScientistRepository.listAllScientist()
+}
